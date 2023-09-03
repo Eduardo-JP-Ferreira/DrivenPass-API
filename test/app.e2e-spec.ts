39b00079a381d53e68571b609379e1e498d0fa1e
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-09-03 20:37:37 -0300

    Tests enviroment configuration

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { PrismaModule } from '../src/prisma/prisma.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, PrismaModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    prisma = await moduleFixture.get(PrismaService);
+    await prisma.user.deleteMany();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('GET /health', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect('I’m okay!');
   });
 });
